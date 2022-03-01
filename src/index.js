af1e7a027a20cf1be092b8842ce3e294a1026861
sebski123 <sebthomle@gmail.com>
2022-03-01 01:21:15 +0100

    part 6 c

--- src/index.js
+++ src/index.js
@@ -1,30 +1,14 @@
-import App from "./App";
-import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
-import { configureStore } from "@reduxjs/toolkit";
-import filterReducer from "./reducers/filterReducer";
-import noteReducer from "./reducers/noteReducer";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./store";
 
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer,
-  },
-});
-
-console.log(store.getState());
-
-const renderApp = () => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-};
-
-renderApp();
-store.subscribe(renderApp);
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
